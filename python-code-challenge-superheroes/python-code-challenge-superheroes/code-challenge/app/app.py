f0e1fb3998706918c93d25d7b5f0387cddc66c46
Wesley <wesochienga@gmail.com>
2023-09-26 00:47:59 +0300

    second commit

--- python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
@@ -1,21 +1,118 @@
-#!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, request, abort
+from models import db, Hero, Power, HeroPower
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db, Hero
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///heroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
-
 @app.route('/')
-def home():
-    return ''
+def home ():
+     return ""
+
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    hero_data = [{'id': hero.id, 'name': hero.name, 'super_name': hero.super_name} for hero in heroes]
+    return jsonify(hero_data)
+
+
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero_by_id(id):
+    hero = Hero.query.get(id)
+    if not hero:
+        return jsonify({"error": "Hero not found"}), 404
+
+    hero_data = {
+        "id": hero.id,
+        "name": hero.name,
+        "super_name": hero.super_name,
+        "powers": [
+            {
+                "id": power.id,
+                "name": power.name,
+                "description": power.description
+            } for power in hero.powers
+        ]
+    }
+    return jsonify(hero_data)
+
+@app.route('/powers', methods=['GET'])
+def get_powers():
+    powers = Power.query.all()
+    power_data = [{'id': power.id, 'name': power.name, 'description': power.description} for power in powers]
+    return jsonify(power_data)
+
+@app.route('/powers/<int:id>', methods=['GET'])
+def get_power_by_id(id):
+    power = Power.query.get(id)
+    if not power:
+        return jsonify({"error": "Power not found"}), 404
+
+    power_data = {
+        "id": power.id,
+        "name": power.name,
+        "description": power.description
+    }
+    return jsonify(power_data)
+
+@app.route('/powers/<int:id>', methods=['PATCH'])
+def update_power(id):
+    power = Power.query.get(id)
+    if not power:
+        return jsonify({"error": "Power not found"}), 404
+
+    form = PowerForm(request.form)
+    if form.validate():
+        description = request.json.get("description")
+        power.description = description
+        db.session.commit()
+        return jsonify({
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        })
+    else:
+        return jsonify({"errors": form.errors}), 400
+
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    form = HeroPowerForm(request.form)
+    if form.validate():
+        power_id = request.json.get("power_id")
+        hero_id = request.json.get("hero_id")
+        strength = request.json.get("strength")
+
+        hero = Hero.query.get(hero_id)
+        power = Power.query.get(power_id)
+
+        if not hero or not power:
+            return jsonify({"error": "Hero or Power not found"}), 404
+
+        hero_power = HeroPower(strength=strength, power=power, hero=hero)
+        db.session.add(hero_power)
+        db.session.commit()
+aoo
+        hero_data = {
+            "id": hero.id,
+            "name": hero.name,
+            "super_name": hero.super_name,
+            "powers": [
+                {
+                    "id": power.id,
+                    "name": power.name,
+                    "description": power.description
+                } for power in hero.powers
+            ]
+        }
+        return jsonify(hero_data), 201
+    else:
+        return jsonify({"errors": form.errors}), 400
+
 
 
 if __name__ == '__main__':
---
+++ python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/seed.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from models import db, Hero, Power, HeroPower
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///heroes.db'
+db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
+
+    
+    power1 = Power(name="Super Strength", description="Gives the wielder super-human strengths")
+    power2 = Power(name="Flight", description="Gives the wielder the ability to fly through the skies at supersonic speed")
+    power3 = Power(name="Telekinesis", description="Allows the wielder to move objects with their mind")
+
+    db.session.add_all([power1, power2, power3])
+    db.session.commit()
+
+    
+    hero1 = Hero(name="Kamala Khan", super_name="Ms. Marvel", powers=[power1, power2])
+    hero2 = Hero(name="Doreen Green", super_name="Squirrel Girl", powers=[power1, power3])
+    hero3 = Hero(name="Gwen Stacy", super_name="Spider-Gwen", powers=[power2])
+
+    db.session.add_all([hero1, hero2, hero3])
+    db.session.commit()
+
+   
+    hero_power1 = HeroPower(hero=hero1, power=power1, strength="Strong")
+    hero_power2 = HeroPower(hero=hero1, power=power2, strength="Average")
+    hero_power3 = HeroPower(hero=hero2, power=power1, strength="Strong")
+    hero_power4 = HeroPower(hero=hero2, power=power3, strength="Average")
+    hero_power5 = HeroPower(hero=hero3, power=power2, strength="Weak")
+
+    db.session.add_all([hero_power1, hero_power2, hero_power3, hero_power4, hero_power5])
+    db.session.commit()
+
+print("Database seeding completed.")
